a456cc3c603c6eaa5f1a67c86024728d4ab922cb
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-04-15 11:07:24 -0700

    Forgot a ;

--- app/src/main/java/com/create/nfraggle/filmjournal/FilmRollCursorAdapter.java
+++ app/src/main/java/com/create/nfraggle/filmjournal/FilmRollCursorAdapter.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
-import com.create.nfraggle.filmjournal.data.FilmContract.FilmRollEntry
+import com.create.nfraggle.filmjournal.data.FilmContract.FilmRollEntry;
 
 import com.create.nfraggle.filmjournal.data.FilmContract;
 
