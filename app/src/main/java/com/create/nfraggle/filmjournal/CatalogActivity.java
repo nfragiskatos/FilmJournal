1db3f43004aa129bcb983105b98b9d45cfd0e38d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-04-17 17:14:25 -0700

    Added code to inflate FilmRollEditorActivity. Created the FAB in the CatalogActivity, as well as added the intent to open the FimlRollEditorActivity

--- app/src/main/java/com/create/nfraggle/filmjournal/CatalogActivity.java
+++ app/src/main/java/com/create/nfraggle/filmjournal/CatalogActivity.java
@@ -1,15 +1,47 @@
 package com.create.nfraggle.filmjournal;
 
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+
 
 import com.create.nfraggle.filmjournal.data.FilmDbHelper;
 
-public class CatalogActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open FilmRollEditActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, FilmRollEditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
--- app/src/main/java/com/create/nfraggle/filmjournal/FilmRollEditorActivity.java
+++ app/src/main/java/com/create/nfraggle/filmjournal/FilmRollEditorActivity.java
@@ -4,14 +4,32 @@ import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by Nicholas Fragiskatos on 4/17/2018.
  */
 public class FilmRollEditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private EditText mDescriptionEditText;
+    private EditText mDateEditText;
+
+    private static final int FILM_ROLL_LOADER = 0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_roll_editor);
+
+        mDescriptionEditText = (EditText) findViewById(R.id.edit_film_roll_description);
+        mDateEditText = (EditText) findViewById(R.id.edit_film_roll_date);
+    }
+
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
         return null;
     }
 
